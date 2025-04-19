c9a5f710cc52653c93922e7180a64bae6e53fe71
Azhan122 <azhan122@ucsc.edu>
2025-04-18 21:43:00 -0700

    Game 1(b): Link-based world implementation

--- rules.js
+++ rules.js
@@ -1,4 +1,4 @@
-class Start extends Scene {
+/*class Start extends Scene {
     create() {
         this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
         this.engine.addChoice("Begin the story");
@@ -41,4 +41,50 @@ class End extends Scene {
     }
 }
 
-Engine.load(Start, 'myStory.json');
+Engine.load(Start, 'myStory.json'); */
+
+class Start extends Scene {
+    create() {
+        this.engine.setTitle(this.engine.storyData.Title); // DONE TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.addChoice("Begin the story");
+    }
+
+    handleChoice() {
+        this.engine.gotoScene(Location, "Your room"); // DONE TODO: replace this text by the initial location of the story
+        // Alyssa: "__" for starting location, can be changed from Kresge to other things
+    } 
+}
+
+class Location extends Scene {
+    create(key) {
+        let locationData = this.engine.storyData.Locations[key]; // Done TODO: use `key` to get the data object for the current story location
+        this.engine.show(locationData.Body); // Done TODO: replace this text by the Body of the location data
+        
+        if(locationData.Choices && locationData.Choices.length > 0) { // Done TODO: check if the location has any Choices
+            for(let choice of locationData.Choices) { // Done TODO: loop over the location's Choices
+                this.engine.addChoice(choice.Text, choice); // Done? TODO: use the Text of the choice
+                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+            }
+        } else {
+            this.engine.addChoice("The end.")
+        }
+    }
+
+    handleChoice(choice) {
+        if(choice) {
+            this.engine.show("&gt; "+choice.Text);
+            this.engine.gotoScene(Location, choice.Target);
+        } else {
+            this.engine.gotoScene(End);
+        }
+    }
+}
+
+class End extends Scene {
+    create() {
+        this.engine.show("<hr>");
+        this.engine.show(this.engine.storyData.Credits);
+    }
+}
+
+Engine.load(Start, 'myStory.json'); 
